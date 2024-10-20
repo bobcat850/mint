553764972f10fe0a9ad1477eeed437111c7baf60
Bobcat <30940862+bobcat850@users.noreply.github.com>
2024-10-20 11:14:42 -0500

    update

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,20 +2,20 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0x4862F372C9B8c02926a9b2a351d2CE71e37C95E4";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = "polygon";
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "a3748812d20783bd4ff2f8a7273ac8d6";
 
 // Configure the primary color for buttons and other UI elements
-export const primaryColorConst = "blue";
+export const primaryColorConst = "green";
 
 // Choose between "light" and "dark" mode
 export const themeConst = "dark";
